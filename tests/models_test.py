db58787b07bd2565dd7ab7d9cf2a0d05799adc33
Gregory Oschwald <goschwald@maxmind.com>
2013-04-09 11:25:14 -0700

    Tests for webservices and models

---
+++ tests/models_test.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import sys
+sys.path.append('..')
+
+import geoip2.models
+import unittest
+
+
+class TestModels(unittest.TestCase):
+
+    def test_omni_full(self):
+        raw = {
+            'city': {
+                'confidence': 76,
+                'geoname_id': 9876,
+                'name': {'en': 'Minneapolis'},
+                },
+            'continent': {
+                'continent_code': 'NA',
+                'geoname_id': 42,
+                'name': {'en': 'North America'},
+                },
+            'country': {
+                'confidence': 99,
+                'geoname_id': 1,
+                'iso_3166_1_alpha_2': 'US',
+                'iso_3166_1_alpha_3': 'USA',
+                'name': {'en': 'United States of America'},
+                },
+            'location': {
+                'accuracy_radius': 1500,
+                'latitude': 44.98,
+                'longitude': 93.2636,
+                'metro_code': 765,
+                'postal_code': '55401',
+                'postal_confidence': 33,
+                'time_zone': 'America/Chicago',
+                },
+            'region': {
+                'confidence': 88,
+                'geoname_id': 574635,
+                'iso_3166_2': 'MN',
+                'name': {'en': 'Minnesota'},
+                },
+            'registered_country': {
+                'geoname_id': 2,
+                'iso_3166_1_alpha_2': 'CA',
+                'iso_3166_1_alpha_3': 'CAN',
+                'name': {'en': 'Canada'},
+                },
+            'traits': {
+                'autonomous_system_number': 1234,
+                'autonomous_system_organization': 'AS Organization',
+                'domain': 'example.com',
+                'ip_address': '1.2.3.4',
+                'is_anonymous_proxy': 0,
+                'is_transparent_proxy': 1,
+                'is_us_military': 1,
+                'isp': 'Comcast',
+                'network_speed': 'cable/DSL',
+                'organization': 'Blorg',
+                'user_type': 'college',
+                },
+            }
+
+        model = geoip2.models.Omni(raw)
+        self.assertEqual(type(model), geoip2.models.Omni,
+                         'geoip2.models.Omni object')
+        self.assertEqual(type(model.city), geoip2.records.City,
+                         'geoip2.records.City object')
+        self.assertEqual(type(model.continent), geoip2.records.Continent,
+                         'geoip2.records.Continent object')
+        self.assertEqual(type(model.country), geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.registered_country),
+                         geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.location), geoip2.records.Location,
+                         'geoip2.records.Location object')
+        self.assertEqual(type(model.region), geoip2.records.Region,
+                         'geoip2.records.Regin object')
+        self.assertEqual(type(model.traits), geoip2.records.Traits,
+                         'geoip2.records.Traits object')
+        self.assertEqual(model.raw, raw, 'raw method returns raw input')
+
+    def test_omni_min(self):
+        model = geoip2.models.Omni({'traits': {'ip_address': '5.6.7.8'}})
+        self.assertEqual(type(model), geoip2.models.Omni,
+                         'geoip2.models.Omni object')
+        self.assertEqual(type(model.city), geoip2.records.City,
+                         'geoip2.records.City object')
+        self.assertEqual(type(model.continent), geoip2.records.Continent,
+                         'geoip2.records.Continent object')
+        self.assertEqual(type(model.country), geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.registered_country),
+                         geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.location), geoip2.records.Location,
+                         'geoip2.records.Location object')
+        self.assertEqual(type(model.region), geoip2.records.Region,
+                         'geoip2.records.Regin object')
+        self.assertEqual(type(model.traits), geoip2.records.Traits,
+                         'geoip2.records.Traits object')
+
+    def test_city_full(self):
+        raw = {
+            'continent': {
+                'continent_code': 'NA',
+                'geoname_id': 42,
+                'name': {'en': 'North America'},
+                },
+            'country': {
+                'geoname_id': 1,
+                'iso_3166_1_alpha_2': 'US',
+                'iso_3166_1_alpha_3': 'USA',
+                'name': {'en': 'United States of America'}, 
+                },
+            'registered_country': {
+                'geoname_id': 2,
+                'iso_3166_1_alpha_2': 'CA',
+                'iso_3166_1_alpha_3': 'CAN',
+                'name': {'en': 'Canada'}, 
+                },
+            'traits': {
+                'ip_address': '1.2.3.4',
+                'is_satellite_provider': True,
+                },
+            }
+        model = geoip2.models.City(raw)
+        self.assertEqual(type(model), geoip2.models.City,
+                         'geoip2.models.City object')
+        self.assertEqual(type(model.city), geoip2.records.City,
+                         'geoip2.records.City object')
+        self.assertEqual(type(model.continent), geoip2.records.Continent,
+                         'geoip2.records.Continent object')
+        self.assertEqual(type(model.country), geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.registered_country),
+                         geoip2.records.Country,
+                         'geoip2.records.Country object')
+        self.assertEqual(type(model.location), geoip2.records.Location,
+                         'geoip2.records.Location object')
+        self.assertEqual(type(model.region), geoip2.records.Region,
+                         'geoip2.records.Regin object')
+        self.assertEqual(type(model.traits), geoip2.records.Traits,
+                         'geoip2.records.Traits object')
+        self.assertEqual(model.raw, raw, 'raw method returns raw input')
+        self.assertEqual(model.continent.geoname_id, 42,
+                         'continent geoname_id is 42')
+        self.assertEqual(model.continent.continent_code, 'NA',
+                         'continent continent_code is NA')
+        self.assertEqual(model.continent.names, {'en': 'North America'},
+                         'continent names is correct')
+        self.assertEqual(model.continent.name, 'North America',
+                         'continent name is correct')
+        self.assertEqual(model.country.geoname_id, 1,
+                         'country geoname_id is 1')
+        self.assertEqual(model.country.iso_3166_1_alpha_2, 'US',
+                         'country iso_3166_1_alpha_2 is US')
+        self.assertEqual(model.country.iso_3166_1_alpha_3, 'USA',
+                         'country iso_3166_1_alpha_3 is USA')
+        self.assertEqual(model.country.names,
+                         {'en': 'United States of America'},
+                         'country names is correct')
+        self.assertEqual(model.country.name, 'United States of America',
+                         'country name is correct')
+        self.assertEqual(model.country.confidence, None,
+                         'country confidence is None')
+        self.assertEqual(model.registered_country.iso_3166_1_alpha_2, 'CA',
+                         'registered_country iso_3166_1_alpha_2 is CA')
+        self.assertEqual(model.registered_country.iso_3166_1_alpha_3, 'CAN',
+                         'registered_country iso_3166_1_alpha_3 is CAN')
+        self.assertEqual(model.registered_country.names,
+                         {'en': 'Canada'},
+                         'registered_country names is correct')
+        self.assertEqual(model.registered_country.name,
+                         'Canada',
+                         'registered_country name is correct')
+        self.assertEqual(model.traits.is_anonymous_proxy, False,
+                         'traits is_anonymous_proxy returns False by default')
+        self.assertEqual(model.traits.is_transparent_proxy, False,
+                         'traits is_anonymous_proxy returns False by default')
+        self.assertEqual(model.traits.is_satellite_provider, True,
+                         'traits is_setellite_provider is True')
+        self.assertEqual(model.raw, raw, 'raw method produces raw output')
+
+    def test_names(self):
+        raw = {
+            'continent': {
+                'continent_code': 'NA',
+                'geoname_id': 42, 
+                'name': {     
+                    'en': 'North America',
+                    'zh-CN': '北美洲',
+                    },
+                },
+            'country': {
+                'geoname_id': 1,   
+                'iso_3166_1_alpha_2': 'US',
+                'iso_3166_1_alpha_3': 'USA',
+                'name': {         
+                    'en': 'United States of America',
+                    'fr': 'États-Unis',
+                    'zh-CN': '美国',
+                    },
+                },
+            'traits': {
+                'ip_address': '1.2.3.4',
+                },
+            }
+
+        model = geoip2.models.Country(raw, languages=['fr', 'zh-CN', 'en'])
+        self.assertEqual(model.continent.name, '北美洲',
+                         'continent name is in Chinese (no French available)')
+        self.assertEqual(model.country.name, 'États-Unis',
+                         'country name is in French')
+
+        model = geoip2.models.Country(raw, languages=['fr', 'de'])
+        self.assertEqual(model.continent.name, None,
+                         'continent name is undef (no fr or de available)')
+        self.assertEqual(model.country.name, 'États-Unis',
+                         'country name is in French')
+
+        model = geoip2.models.Country(raw, languages=['de'])
+        self.assertEqual(model.continent.name, None,
+                         'continent name is undef (no German available)')
+        self.assertEqual(model.country.name, None,
+                         'country name is in None (no German available)')
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/webservices_test.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import sys
+sys.path.append('..')
+
+import geoip2
+import json
+import requests
+import unittest
+from geoip2.errors import GeoIP2Error, GeoIP2HTTPError, GeoIP2WebServiceError
+from geoip2.webservices import Client
+
+try:
+    from unittest.mock import MagicMock, patch
+except ImportError:
+    from mock import MagicMock, patch
+
+if sys.version_info[0] == 2:
+    unittest.TestCase.assertRaisesRegex = unittest.TestCase.assertRaisesRegexp
+
+
+@patch.object(requests, 'get')
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client(42, 'abcdef123456',)
+
+    country = {
+        'continent': {
+            'continent_code': 'NA',
+            'geoname_id': 42,
+            'name': { 'en': 'North America' }
+            },
+        'country': {
+            'geoname_id': 1,
+            'iso_3166_1_alpha_2': 'US',
+            'iso_3166_1_alpha_3': 'USA',
+            'name': { 'en': 'United States of America'}
+            },
+        'traits': {'ip_address': '1.2.3.4',},
+        }
+
+    def _setup_get(self, get, endpoint='country', status=200, body=None,
+                   raw_body=None):
+        if body and raw_body:
+            raise ValueError('Both body and raw_body cannot be set')
+        r = requests.Response()
+        r.status_code = status
+        if status == 200 or ( status >= 400 and status < 500 ):
+            r.headers = {'content-type': ('application/vnd.maxmind.com-%s+'
+                                          'json; charset=UTF-8; version=1.0'
+                                          % (endpoint,))}
+        # Monkey-patching .json. A bit ugly.
+        func_type = type(r.json)
+        if body:
+            # XXX - ugly.
+            r._content = json.dumps(body)
+            r.json = func_type(lambda x: body, r)
+        else:
+            if raw_body:
+                r._content = raw_body
+            def raise_exception(*args):
+                raise ValueError('test')
+            r.json = func_type(raise_exception, r)
+
+        get.return_value = r
+
+    def test_country_ok(self, get):
+        self._setup_get(get, 'country', 200, self.country)
+        country = self.client.country('1.2.3.4')
+        self.assertEqual(type(country), geoip2.models.Country,
+                          'return value of client.country')
+        self.assertEqual(country.continent.geoname_id, 42,
+                         'continent geoname_id is 42')
+        self.assertEqual(country.continent.continent_code, 'NA',
+                         'continent continent_code is NA')
+        self.assertEqual(country.continent.name, 'North America',
+                         'continent name is North America')
+        self.assertEqual(country.country.geoname_id, 1,
+                         'country geoname_id is 1')
+        self.assertEqual(country.country.iso_3166_1_alpha_2, 'US',
+                         'country iso_3166_1_alpha_2 is US')
+        self.assertEqual(country.country.iso_3166_1_alpha_3, 'USA',
+                         'country iso_3166_1_alpha_3 is USA')
+        self.assertEqual(country.country.names,
+                         { 'en': 'United States of America' },
+                         'country names' )
+        self.assertEqual(country.country.name, 'United States of America',
+                         'country name is United States of America')
+        self.assertEqual(country.raw, self.country, 'raw response is correct')
+
+    def test_me(self, get):
+        self._setup_get(get, body=self.country)
+        implicit_me = self.client.country()
+        self.assertEqual(type(implicit_me), geoip2.models.Country,
+                          'country() returns Country object')
+        explicit_me = self.client.country()
+        self.assertEqual(type(explicit_me), geoip2.models.Country,
+                          'country(\'me\') returns Country object')
+
+    def test_200_error(self, get):
+        self._setup_get(get)
+        with self.assertRaisesRegex(GeoIP2Error,
+                                     'could not decode the response as JSON'):
+            self.client.country('1.1.1.1')
+
+    def test_400_error(self, get):
+        body = {'code': 'IP_ADDRESS_INVALID',
+                'error': 'The value "1.2.3" is not a '
+                'valid ip address',}
+        self._setup_get(get, status=400, body=body)
+        with self.assertRaisesRegex(GeoIP2WebServiceError,
+                                     'The value "1.2.3" is not a valid '
+                                    'ip address'):
+            self.client.country('1.2.3')
+        # XXX - there might be a better way to do this
+        try:
+            self.client.country('1.2.3')
+        except GeoIP2WebServiceError as e:
+            self.assertEqual(e.http_status, 400,
+                             'exception object contains expected http_status'
+                             )
+            self.assertEqual(e.code,'IP_ADDRESS_INVALID',
+                             'exception object contains expected code'
+                             )
+
+    def test_no_body_error(self, get):
+        self._setup_get(get, status=400)
+        with self.assertRaisesRegex(GeoIP2HTTPError,
+                                    'Received a 400 error for .* with no body'):
+            self.client.country('1.2.3.7')
+        
+    def test_weird_body_error(self, get):
+        self._setup_get(get, status=400, body={ 'weird': 42 },)
+        with self.assertRaisesRegex(GeoIP2HTTPError,
+                                    'Response contains JSON but it does not '
+                                    'specify code or error keys'):
+            self.client.country('1.2.3.8')
+
+    def test_bad_body_error(self, get):
+        self._setup_get(get, status=400, raw_body='bad body')
+        with self.assertRaisesRegex(GeoIP2HTTPError,
+                                    'it did not include the expected JSON body'
+                                    ):
+            self.client.country('1.2.3.9')
+
+    def test_500_error(self, get):
+        self._setup_get(get, status=500)
+        with self.assertRaisesRegex(GeoIP2HTTPError,
+                                    'Received a server error \(500\) for'):
+            self.client.country('1.2.3.10')
+
+    def test_300_error(self, get):
+        self._setup_get(get, status=300)
+        with self.assertRaisesRegex(GeoIP2HTTPError,
+                                    'Received a very surprising HTTP status '
+                                    '\(300\) for'):
+
+            self.client.country('1.2.3.11')
+
+    def test_request(self, get):
+        self._setup_get(get, 'country', 200, self.country)
+        country = self.client.country('1.2.3.4')
+        get.assert_called_with('https://geoip.maxmind.com'
+                               '/geoip/country/1.2.3.4',
+                               headers={'Accept': 'application/json'},
+                               auth=(42, 'abcdef123456'))
+
+if __name__ == '__main__':
+    unittest.main()
