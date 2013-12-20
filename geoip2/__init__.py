2b0f0e91799647a7afdc3f34ea9e5a3a51b4ddef
Gregory Oschwald <goschwald@maxmind.com>
2013-12-20 07:10:29 -0800

    Prepare for 0.4.2

--- geoip2/__init__.py
+++ geoip2/__init__.py
@@ -1,7 +1,7 @@
 # pylint:disable=C0111
 
 __title__ = 'geoip2'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 __author__ = 'Gregory Oschwald'
 __license__ = 'LGPLv2.1+'
 __copyright__ = 'Copyright 2013 Maxmind, Inc.'
