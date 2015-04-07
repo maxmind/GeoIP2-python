4346d46b5c371d9324811a26a0a20f45c4a27650
Gregory Oschwald <goschwald@maxmind.com>
2015-04-07 15:43:15 -0700

    Remove bugtrack_url as not all tools support it, causing warnings

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
     author='Gregory Oschwald',
     author_email='goschwald@maxmind.com',
     url='http://www.maxmind.com/',
-    bugtrack_url='https://github.com/maxmind/GeoIP2-python/issues',
     packages=['geoip2'],
     package_data={'': ['LICENSE']},
     package_dir={'geoip2': 'geoip2'},
