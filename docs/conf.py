a4951f5a23caa41c9b840c4c73a53d2ee2f8e4fe
Gregory Oschwald <goschwald@maxmind.com>
2018-01-18 07:58:03 -0800

    Bump copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'geoip2'
-copyright = '2013-2016, MaxMind, Inc.'
+copyright = '2013-2018, MaxMind, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
