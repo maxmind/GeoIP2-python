fec4c38666e9e50293986c690188e098b4af3f39
Gregory Oschwald <goschwald@maxmind.com>
2020-02-19 10:11:25 -0800

    Update CI to use black

--- .travis-black.sh
+++ .travis-black.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-diff=$(yapf -rd geoip2 tests)
+diff=$(black --check .)
 
 if [[ $? != 0 ]]; then
-    echo "yapf failed to run."
+    echo "black failed to run."
     echo "$diff"
     exit $?
 elif [[ $diff ]]; then
