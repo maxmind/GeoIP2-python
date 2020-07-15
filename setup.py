0e35405ff6fd4e7f22d34ba9f88ed17acf9b0ccb
Gregory Oschwald <goschwald@maxmind.com>
2020-07-15 14:01:17 -0700

    Require mocket for tests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=requirements,
-    tests_require=["httpretty>=1.0.0"],
+    tests_require=["mocket>=3.8.6"],
     test_suite="tests",
     license=geoip2.__license__,
     classifiers=[
