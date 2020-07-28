77c926d8d981d1375ecf3c1497ec5d1dd0d2709e
Gregory Oschwald <goschwald@maxmind.com>
2020-07-28 10:38:54 -0700

    Update for v4.0.2

--- geoip2/__init__.py
+++ geoip2/__init__.py
@@ -1,7 +1,7 @@
 # pylint:disable=C0111
 
 __title__ = "geoip2"
-__version__ = "4.0.1"
+__version__ = "4.0.2"
 __author__ = "Gregory Oschwald"
 __license__ = "Apache License, Version 2.0"
 __copyright__ = "Copyright (c) 2013-2020 Maxmind, Inc."
