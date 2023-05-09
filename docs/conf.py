f8608c2ff03aa5a3b46e004eaaf075ed62e4f2e9
Gregory Oschwald <goschwald@maxmind.com>
2023-05-09 14:25:42 -0700

    Fix some sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ html_theme = "sphinxdoc"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -246,4 +246,6 @@ texinfo_documents = [
 # texinfo_show_urls = 'footnote'
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"http://docs.python.org/": None}
+intersphinx_mapping = {
+    "python": ("https://python.readthedocs.org/en/latest/", None),
+}
