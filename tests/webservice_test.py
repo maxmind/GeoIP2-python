002a692094279374b76ce29f939e154fa8801a2c
Aarni Koskela <akx@iki.fi>
2024-08-27 10:27:12 +0300

    Make aiohttp and requests optional dependencies
    Closes #104 (supersedes it)
    
    Fixes #101

--- tests/webservice_test.py
+++ tests/webservice_test.py
@@ -400,6 +400,7 @@ class TestClient(TestBaseClient):
     client: Client
 
     def setUp(self) -> None:
+        pytest.importorskip("requests")
         self.client_class = Client
         self.client = Client(42, "abcdef123456")
         self.client._base_uri = self.httpserver.url_for("/geoip/v2.1")  # noqa: SLF001
@@ -413,6 +414,7 @@ class TestAsyncClient(TestBaseClient):
     client: AsyncClient
 
     def setUp(self) -> None:
+        pytest.importorskip("aiohttp")
         self._loop = asyncio.new_event_loop()
         self.client_class = AsyncClient
         self.client = AsyncClient(42, "abcdef123456")
